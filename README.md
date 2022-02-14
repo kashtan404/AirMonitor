667adf7896d3e8e5812f07763c2e3260a53d5c9a
Aleksei Demidov <al.kashtan.ex@gmail.com>
2022-02-14 23:19:28 +0300

    add airmonitor firmware

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# AirMonitor
+TBD
+# Arduino air montor
+
+## Description
+Small air monitor with CO2, temperature and humidity sensors.
+
+## Directories
+- **firmware** - Arduino firmware
+- **schemes** - wiring schemes
+
+## Schemes
+TBD
+
+## Components
+- Arduino NANO https://aliexpress.ru/item/4000587268145.html
+- AHT10 (5V) https://aliexpress.ru/item/33056637682.html
+- MH-Z19 https://aliexpress.ru/item/4000212024923.html
+- LCD 1602 i2c https://aliexpress.ru/item/32763867041.html
+
+Misc:
+- LED (pack) https://aliexpress.ru/item/32780378991.html
+- Resistor (220 R) https://aliexpress.ru/item/4001066921637.html
+- Photoconductive resistor https://aliexpress.ru/item/32623615207.html
+- Wires https://aliexpress.ru/item/32872439317.html
+- Body https://www.chipdip.ru/product/g403
+
+
+## Dependencies
+* [Adafruit_AHTX0](https://github.com/adafruit/Adafruit_AHTX0)
+* [Adafruit_Sensor](https://github.com/adafruit/Adafruit_Sensor)
+* [MHZ19_uart](https://github.com/piot-jp-Team/mhz19_uart)
+* [LiquidCrystal_I2C](https://github.com/johnrickman/LiquidCrystal_I2C)
+* [GyverTimer](https://github.com/GyverLibs/GyverTimer)
+
+
+## Variables
+    SENS_TIME 30000       // sensors read period
+    BRIGHT_THRESHOLD 550  // photoresistor threshold to switch brightness to min (0-1023)
+    LCD_BRIGHT_MAX 255    // brightness level max
+    LCD_BRIGHT_MIN 10     // brightness level min
+    DISPLAY_ADDR 0x27     // display adress
+    BACKLIGHT 10          // display backlight pin
+    PHOTO A3              // photoresistor pin
+    MHZ_RX 2              // co2 sensor TX pin 
+    MHZ_TX 3              // co2 sensor RX pin
+    LED 5                 // LED pin
+
---
+++ firmware/air_monitor/air_monitor.ino
@@ -0,0 +1,112 @@
+#define SENS_TIME 30000       // sensors read period
+#define BRIGHT_THRESHOLD 550  // photoresistor threshold to switch brightness to min (0-1023)
+#define LCD_BRIGHT_MAX 255    // brightness level max
+#define LCD_BRIGHT_MIN 10     // brightness level min
+#define DISPLAY_ADDR 0x27     // display adress
+#define BACKLIGHT 10          // display backlight pin
+#define PHOTO A3              // photoresistor pin
+#define MHZ_RX 2              // co2 sensor TX pin 
+#define MHZ_TX 3              // co2 sensor RX pin
+#define LED 5                 // LED pin
+
+// display
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(DISPLAY_ADDR, 16, 2);
+
+// temp+humidity sensor
+#include <Adafruit_Sensor.h>
+#include <Adafruit_AHTX0.h>
+Adafruit_AHTX0 aht;
+
+// co2 sensor
+#include <MHZ19_uart.h>
+MHZ19_uart mhz19;
+
+// timers
+#include <GyverTimer.h>
+GTimer sensorsTimer(MS, SENS_TIME);
+GTimer drawSensorsTimer(MS, SENS_TIME);
+GTimer brightTimer(MS, 2000);
+GTimer co2LEDTimer(MS, 2000);
+
+// output vars
+float dispTemp;
+byte dispHum;
+int dispCO2;
+
+// LED blink vars
+long interval = 1000;
+long previousInterval = 0;
+
+void checkBrightness() {
+  if (analogRead(PHOTO) < BRIGHT_THRESHOLD) {
+    analogWrite(BACKLIGHT, LCD_BRIGHT_MIN);
+  } else {
+    analogWrite(BACKLIGHT, LCD_BRIGHT_MAX);
+  }
+}
+
+void readSensors() {
+  sensors_event_t humidity, temp;
+  aht.getEvent(&humidity, &temp);
+  dispTemp = temp.temperature;
+  dispHum = humidity.relative_humidity;
+  dispCO2 = mhz19.getPPM();
+}
+
+void drawSensors() {
+  lcd.setCursor(0, 0);
+  lcd.print("CO2: " + String(dispCO2) + "ppm ");
+  lcd.setCursor(13, 0);
+  if (dispCO2 < 700) lcd.print("OK ");
+  else lcd.print("WRN");
+  lcd.setCursor(0, 1);
+  lcd.print("t: " + String(dispTemp, 1));
+  lcd.write(223);
+  lcd.setCursor(9, 1);
+  lcd.print("h: " + String(dispHum) + "% ");
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(BACKLIGHT, OUTPUT);
+  pinMode(LED, OUTPUT);
+
+  digitalWrite(LED, 0);
+  analogWrite(BACKLIGHT, LCD_BRIGHT_MAX);
+
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+
+  mhz19.begin(MHZ_TX, MHZ_RX);
+  mhz19.setAutoCalibration(false);
+  aht.begin(&Wire);
+
+  readSensors();
+  drawSensors();
+}
+
+void loop() {
+  if (brightTimer.isReady()) checkBrightness();
+  if (sensorsTimer.isReady()) readSensors();
+  if (drawSensorsTimer.isReady()) drawSensors();
+
+  if (dispCO2 < 700) {
+    co2LEDTimer.stop();
+    digitalWrite(LED, 0);
+    previousInterval = 0;
+  }
+  else {
+    if (dispCO2 < 1000) interval = 1000;
+    else if (dispCO2 < 1200) interval = 600;
+    else if (dispCO2 >= 1500) interval = 300;
+    if (previousInterval != interval) {
+      previousInterval = interval;
+      co2LEDTimer.setInterval(interval);
+    }
+    if (co2LEDTimer.isReady()) digitalWrite(LED, !(digitalRead(LED)));
+  }
+}
